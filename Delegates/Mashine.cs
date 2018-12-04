a8a9ed2bd0a0c16c6a3541b8cb06e13d0f416380
Bojan Bogdanovski <freeboy_bb@yahoo.co.uk>
2018-12-05 00:54:14 +0100

    Add project files.

---
+++ Delegates/Mashine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Delegates
+{
+    public delegate void TempChanged(double prev, double current);
+
+    public class Mashine
+    {
+        private double _currentTemp = 36;
+        private TempChanged _tempChanged;
+
+        public double CurrentTemp
+        {
+            get => _currentTemp;
+
+            set
+            {
+                if (_currentTemp != value)
+                {
+                    var prev = _currentTemp;
+                    _currentTemp = value;
+                    OnTempChanged(prev, value);
+                }
+            }
+        }
+
+        public void TurnOn()
+        {
+            while (CurrentTemp < 100)
+            {
+                Thread.Sleep(1000);
+                CurrentTemp++;
+            }
+        }
+
+        /// <summary>
+        /// Subscribe to Event with the delegate
+        /// </summary>
+        /// <param name="watcher">Delegate to Execute</param>
+        public void RegisterTempChange(TempChanged watcher)
+        {
+            _tempChanged += watcher;
+        }
+
+        /// <summary>
+        /// Raise the Event that is subscribed
+        /// </summary>
+        /// <param name="prev"></param>
+        /// <param name="current"></param>
+        private void OnTempChanged(double prev, double current)
+        {
+            _tempChanged?.Invoke(prev, current);
+        }
+    }
+
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mashine m = new Mashine();
+
+            //m.RegisterTempChange(LogToConsole);
+            m.RegisterTempChange((prev, current) =>
+            {
+                Console.WriteLine($"Temp changed from {prev} to {current}");
+            });
+
+            //m.RegisterTempChange(LogToFile);
+            m.RegisterTempChange((prev, current) =>
+            {
+                using (StreamWriter writter = new StreamWriter("temps.txt", true))
+                {
+                    writter.WriteLineAsync($"Temp changed from {prev} to {current}");
+                }
+            });
+
+            m.TurnOn();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void LogToConsole(double prev, double current)
+        {
+            Console.WriteLine($"Temp changed from {prev} to {current}");
+        }
+
+        private static void LogToFile(double prev, double current)
+        {
+            using (StreamWriter writter = new StreamWriter("temps.txt", true))
+            {
+                writter.WriteLineAsync($"Temp changed from {prev} to {current}");
+            }
+
+        }
+    }
+}
